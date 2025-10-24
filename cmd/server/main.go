ffbf02cdadae414843a8ac5e7807af49551e370f
marcos-j-ferreira <marcos.j.lemes.2004@gmail.com>
2025-10-24 15:02:13 -0300

    feat: api in Golang

---
+++ cmd/server/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"ci/cd/internal/handler"
+	"fmt"
+)
+
+
+func main() {
+	gin.SetMode(gin.ReleaseMode)
+
+	r := gin.New()
+
+	r.Use(gin.Recovery())
+
+	r.GET("/", handler.HandlerHello)
+
+	fmt.Println("Servidor ouvindo na porta 8080...")
+
+	r.Run(":8080")
+}
